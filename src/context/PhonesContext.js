d2d4bbc1298a140449ef1ff30c2f2dcc588018e0
Lokendra Puri <lpuri1979@gmail.com>
2022-07-14 17:51:25 +0100

    add routes

---
+++ src/context/PhonesContext.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhonesContext = () => {
+  return (
+    <div>PhonesContext</div>
+  )
+}
+
+export default PhonesContext
---
+++ src/pages/AllPhonesPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { useContext } from 'react'
+
+const AllPhonesPage = () => {
+
+  return (
+    <div>AllPhonesPage</div>
+  )
+}
+
+export default AllPhonesPage
